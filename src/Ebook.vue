da748a82a6a30c680427df134a0a2521339d6dad
lzw000 <1206678340@qq.com>
2019-11-30 14:47:14 +0800

    电子书的解析和渲染、翻页

---
+++ src/Ebook.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="ebook">
+    <div class="read-wrapper">
+      <div id="read"></div>
+      <div class="mask">
+        <div class="left" @click="prevPage"></div>
+        <div class="center"></div>
+        <div class="right" @click="nextPage"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Epub from 'epubjs'
+const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub'
+global.ePub = Epub
+export default {
+  methods: {
+    prevPage () {
+      if (this.rendition) {
+        this.rendition.prev()
+      }
+    },
+    nextPage () {
+      if (this.rendition) {
+        this.rendition.next()
+      }
+    },
+    // 电子书的解析和渲染
+    showEpub () {
+      // 生成 Book
+      this.book = new Epub(DOWNLOAD_URL)
+      // 生成Rendition，通过book对象的renderTo方法
+      this.rendition = this.book.renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      // 通过rendition.display渲染电子书
+      this.rendition.display()
+    }
+  },
+  mounted () {
+    this.showEpub()
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import 'assets/styles/global';
+.ebook {
+  position: relative;
+  .read-wrapper {
+    .mask {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 100;
+      display: flex;
+      width: 100%;
+      height: 100%;
+      .left {
+        flex: 0 0 px2rem(100);
+      }
+      .center {
+        flex: 1;
+      }
+      .right {
+        flex: 0 0 px2rem(100);
+      }
+    }
+  }
+}
+</style>
